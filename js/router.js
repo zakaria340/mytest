5480275c6555e80069772eda5ebe9d091b9f96f7
Zakaria Elhariri <zakaria-elhariri@smile-maroc.com>
2016-03-08 10:53:45 +0000

    paginator

--- js/router.js
+++ js/router.js
@@ -7,7 +7,7 @@ define([
             'search/:sourceId(/)(:term)(/:ville)(/:tags)(/:order)(/:page)': 'searchImages'
         },
         searchImages: function (sourceId, term, ville, tags, order, page) {
-          console.log(page);
+          console.log(term);
             app.appQuery.set({sourceId: sourceId, term: term, ville: ville, tags: tags, order: order, page: page});
         }
     });
